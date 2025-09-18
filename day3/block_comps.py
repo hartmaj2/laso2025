e91978f4eb87fb659ab789a386a61578fd7906d5
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2025-09-18 14:14:22 +0200

    Fix program

--- day3/block_comps.py
+++ day3/block_comps.py
@@ -1,16 +1,18 @@
 """
 https://projecteuler.net/problem=114
-
-TODO: fix this
 """
 
 a = [1]
-max = 7
+max = 50
 
-for n in range(0,max):
+for n in range(0,max+1):
     a.append(0)
-    a[n+1] = a[n]+sum([a[n-d-1] for d in range(3,n)])
-    if n + 1 >= 3:
-        a[n+1] += 1
+    a[n+1] += a[n] # if n+1-th cell is an empty cell
 
-print(a)
+    # d is not the full size of the brick but the amount of cells it covers from the n-celled area
+    a[n+1] += sum([a[n-d-1] for d in range(2,n)]) # we have d cells taken from the full length of n+1 and also we have to leave one empty cell on the left of end of the brick
+    if n + 1 >= 3:
+        a[n+1] += 1 # we can also fit a full brick inside our designated area
+
+
+print(a[max])
